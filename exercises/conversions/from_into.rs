f36e4fe10760bd9369edaee3da0aade11b271137
YZ775 <yuzuki725.m@gmail.com>
2024-02-15 19:12:34 +0900

    finish clippy and conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        if let Some((name, age)) = s.split_once(',') {
+            if name.is_empty() {
+                return Person::default();
+            }
+            if let Ok(x) = age.parse::<usize>() {
+                return Person {
+                    name: name.to_string(),
+                    age: x,
+                };
+            } else {
+                return Person::default();
+            }
+        } else {
+            return Person::default();
+        }
     }
 }
 
